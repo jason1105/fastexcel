ebb5b96e4c30dfa10856be3a6dfab4c646252320
lv-wei <lvwei5@lenovo.com>
2019-01-20 16:56:45 +0800

    Add function to convert data of excel in database

---
+++ src/test/java/com/github/sd4324530/fastexcel/entity/DBMenu.java
@@ -0,0 +1,15 @@
+package com.github.sd4324530.fastexcel.entity;
+
+public class DBMenu {
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    Integer id;
+
+
+}
